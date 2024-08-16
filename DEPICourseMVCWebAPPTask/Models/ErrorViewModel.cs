62f7f4c0f25b2ef6c6278305ff3eefb0c9557ecc
MostafaPro0 <MostafaPro0@yahoo.com>
2024-08-16 14:51:11 +0300

    aaa

--- DEPICourseMVCWebAPPTask/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DEPICourseMVCWebAPPTask.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
