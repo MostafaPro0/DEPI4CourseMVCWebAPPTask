eab9a25d1d42b13b5eef61de09bc333aa3a7e8d2
MostafaPro0 <MostafaPro0@yahoo.com>
2024-08-16 14:44:28 +0300

    Edit Name

---
+++ DEPICourseMVCWebAPPTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DEPICourseMVCWebAPPTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- DEPICourseWebAPPTask/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DEPICourseWebAPPTask.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
